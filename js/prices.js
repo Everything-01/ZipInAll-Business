9533f689ad8d8a98a9fdf3b5565fe9c4e1b86d86
Everything-01 <everthingecom@gmail.com>
2026-01-19 15:15:34 +0530

    Update prices.js

--- js/prices.js
+++ js/prices.js
@@ -1,1 +1,23 @@
+const prices = [
+  {
+    name: "Vijaya Karnataka",
+    basePrice: 210,
+    zipinallPrice: 220
+  },
+  {
+    name: "Vijavani",
+    basePrice: 200,
+    zipinallPrice: 210
+  },
+  {
+    name: "Udayavani",
+    basePrice: 190,
+    zipinallPrice: 200
+  },
+  {
+    name: "Prajavani",
+    basePrice: 220,
+    zipinallPrice: 230
+  }
+];
 
