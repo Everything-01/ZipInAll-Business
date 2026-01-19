d67793be181701687d7a6f505d0342722d9dcec3
Everything-01 <everthingecom@gmail.com>
2026-01-19 15:20:47 +0530

    Update renderprices.js

--- js/renderprices.js
+++ js/renderprices.js
@@ -1,1 +1,15 @@
+const priceContainer = document.getElementById("price-list");
+
+prices.forEach(paper => {
+  const div = document.createElement("div");
+  div.className = "price-card";
+
+  div.innerHTML = `
+    <h3>${paper.name}</h3>
+    <p>Monthly Price: ₹${paper.zipinallPrice}</p>
+    <small>Includes ₹10 Zipinall service charge</small>
+  `;
+
+  priceContainer.appendChild(div);
+});
 
